87f79d128f2283d8b8abffbec7f9fe6f014291e1
Igor Peco <peca_sk@hotmail.com>
2013-10-29 20:52:12 +0100

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picture::Application.config.secret_key_base = '5bdeec2e45dda661578837df8629a1edfd5029684b2b640f1d4fe72108d9bab4e7893c415520cd1694041f8c9c0c6d58353fec41483abbcf4f42f7243357ce78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Picture::Application.config.session_store :cookie_store, key: '_picture_session'
